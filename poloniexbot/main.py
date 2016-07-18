f7c6d283e262f1d3bb40a0b94739efb0f4b334d0
Andrey Samokhvalov <andrew.shvv@gmail.com>
2016-07-18 13:57:33 +0300

    add usage of absortium api

--- poloniexbot/main.py
+++ poloniexbot/main.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 
+from django.conf import settings
+
+from absortium.client import Client
 from core.utils.logging import getPrettyLogger
 from poloniex.app import Application
 from poloniexbot import constants
@@ -32,14 +35,24 @@ def convert(order):
     }
 
 
+absortium_client = Client(api_key=settings.ABSORTIUM_API_KEY,
+                          api_secret=settings.ABSORTIUM_API_SECRET,
+                          base_api_uri="http://docker.backend")
+
+
 class PoloniexApp(Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
     @staticmethod
     def updates_handler(**update):
-        if update.get('type') in [constants.POLONIEX_OFFER_REMOVED, constants.POLONIEX_OFFER_MODIFIED]:
-            order = update.get("data")
-            order["pair"] = update.get("currency_pair")
 
-            order = convert(order)
+        if i % 5 == 0:
+            if update.get('type') in [constants.POLONIEX_ORDER_REMOVED, constants.POLONIEX_ORDER_MODIFIED]:
+                order = update.get("data")
+                order["pair"] = update.get("currency_pair")
+
+                order = convert(order)
 
     @staticmethod
     def synchronize_orders(orders):
@@ -53,20 +66,17 @@ class PoloniexApp(Application):
                 }
 
                 order = convert(order)
-                # safe_offer_update(price=offer["price"],
-                #                   pair=offer["pair"],
-                #                   order_type=offer["type"],
-                #                   system=offer["system"],
-                #                   update=lambda *args: offer["amount"])
+                absortium_client.order.create(**order)
 
         sync(orders, "bids")
         sync(orders, "asks")
 
     async def main(self):
+        logger.debug(absortium_client.order.list())
 
-        # Subscribe on order update to keep offers synchronized with Poloniex.
-        self.push_api.subscribe(topic=CURRENCY_PAIR, handler=PoloniexApp.updates_handler)
-
-        # Download all Poloniex orders; convert them to offers; add them to the system.
-        orders = await self.public_api.returnOrderBook(currencyPair=CURRENCY_PAIR, depth=COUNT)
-        PoloniexApp.synchronize_orders(orders)
+        # # Subscribe on order update to keep offers synchronized with Poloniex.
+        # self.push_api.subscribe(topic=CURRENCY_PAIR, handler=PoloniexApp.updates_handler)
+        #
+        # # Download all Poloniex orders; convert them to offers; add them to the system.
+        # orders = await self.public_api.returnOrderBook(currencyPair=CURRENCY_PAIR, depth=COUNT)
+        # PoloniexApp.synchronize_orders(orders)
